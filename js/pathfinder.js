0f53afe7de8f5cb2ac7d3f3b69e2d49816989ede
Armen <armen138@gmail.com>
2012-08-18 20:40:18 -0400

    thread pool: now actually pooling threads.

--- js/pathfinder.js
+++ js/pathfinder.js
@@ -1,11 +1,27 @@
 var pathFinder = (function() {
-	var worker = new Worker("js/astar.js"),
+	var available = [],
+	//worker = new Worker("js/astar.js"),
+		MAX_WORKERS = 16,
 		queue = [],
 		busy = false;
+	for(var i = 0; i < MAX_WORKERS; i++) {
+		available.push(new Worker("js/astar.js"));
+	}
 	return {
 		find: function(start, end, callback) {
-			if(!busy) {
-				busy = true;
+			if(available.length > 0) {
+				(function(worker, start, end, callback) {
+					worker.postMessage({ collisionMap: game.collisionMap, x1: start.X, y1: start.Y, x2: end.X, y2: end.Y});
+					worker.onmessage = function(e) {
+						callback(e.data);
+						available.push(worker);
+						if(queue.length > 0) {
+							var n = queue.shift();
+							pathFinder.find(n.start, n.end, n.cb);
+						}
+					};
+				}(available.shift(), start, end, callback));
+/*				busy = true;
 				worker.postMessage({ collisionMap: game.collisionMap, x1: start.X, y1: start.Y, x2: end.X, y2: end.Y});
 				worker.onmessage = function(e) {
 					callback(e.data);
@@ -14,7 +30,7 @@ var pathFinder = (function() {
 						var n = queue.shift();
 						pathFinder.find(n.start, n.end, n.cb);
 					}
-				};
+				};*/
 			} else {
 				queue.push({ start: start, end: end, cb: callback });
 			}
